f7b6e08bc0fce9e73c7516363e0c598c5b0a8949
ShahabBakht <shahab.bakhtiari@gmail.com>
2015-06-30 10:59:01 -0400

    edited ModelStimulator

--- ModelStimulator.m
+++ ModelStimulator.m
@@ -1,2 +1,20 @@
 % open the stimulator image
 
+FigurePath = 'C:\Users\labuser\Google Drive\newResearch\Projects\Ibis Stereotactic Frame to MRI';
+FigureName = 'Stimulator_long.png';
+
+StimulatorImage = imread([FigurePath,'\',FigureName]);
+StimulatorImage = rgb2gray(StimulatorImage);
+
+% set the mm scale on the image
+figure;imagesc(StimulatorImage);
+[x,y] = ginput(2);
+if abs(x(1) - x(2)) > abs(y(1) - y(2))
+    mm_scale = abs(x(1) - x(2));
+else
+    mm_scale = abs(y(1) - y(2));
+end
+
+% 
+
+
